a70908ad8fb53e83adcadaaa1f81aee0c690fbfb
unilock <unilock@fennet.rentals>
2023-03-24 21:42:52 -0400

    thanks
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Do Not Tilt The Camera In Relation To Damage Direction
+
+un-fix the directional damage camera tilt """fix""" in >=1.19.4
---
+++ src/main/java/rocks/katiekatiekatie/donottiltthecamerainrelationtodamagedirection/mixin/GameRendererMixin.java
@@ -0,0 +1,15 @@
+package rocks.katiekatiekatie.donottiltthecamerainrelationtodamagedirection.mixin;
+
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+    @Redirect(method = "tiltViewWhenHurt(Lnet/minecraft/client/util/math/MatrixStack;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getDamageTiltYaw()F"))
+    private float redirect(LivingEntity instance) {
+        return 0;
+    }
+}
