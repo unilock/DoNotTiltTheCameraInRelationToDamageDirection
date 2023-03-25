5331ef449164be06618ee0ccd6b485055530ed87
unilock <unilock@fennet.rentals>
2023-03-24 20:48:36 -0400

    why are you making me do this
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/rocks/katiekatiekatie/donottiltthecamerainrelationtodamagedirection/DoNotTiltTheCameraInRelationToDamageDirection.java
@@ -0,0 +1,9 @@
+package rocks.katiekatiekatie.donottiltthecamerainrelationtodamagedirection;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DoNotTiltTheCameraInRelationToDamageDirection implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
